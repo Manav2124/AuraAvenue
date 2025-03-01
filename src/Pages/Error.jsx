05797065ec8630f01f64ee49794be59349db5e56
Manav2124 <2421manav@gmail.com>
2025-03-01 11:59:14 +0530

    i did-changes-in

--- src/Pages/Error.jsx
+++ src/Pages/Error.jsx
@@ -3,6 +3,6 @@ import error from "../Assets/Images/20602777_6325257.jpg"
 
 export default function Error() {
   return <div>
-    <img src={error} alt="" />
+    <img src={error} alt="hello 01-03-2025-m" />
   </div>
 }
